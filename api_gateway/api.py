df13bf277ad9c184b69c1f115e1f254abc755405
Danielcyh <danielcheeyh@hotmail.com>
2026-08-08 22:53:56 +0800

    Updated with Predict URL

--- api_gateway/api.py
+++ api_gateway/api.py
@@ -5,26 +5,35 @@ import requests
 app = Flask(__name__)
 CORS(app) 
 
-# The internal Docker network URLs to reach your Preprocessor
 PREPROCESSOR_URL = "http://preprocessor-service:5001/clean"
 PREPROCESSOR_CSV_URL = "http://preprocessor-service:5001/process-csv"
+PREDICTION_URL = "http://prediction-service:5002/predict"
 
-# --- Handles the Manual Input Form ---
-@app.route('/submit-transaction', methods=['POST'])
-def submit_transaction():
+@app.route('/predict', methods=['POST'])
+def overall_api():
     try:
         raw_data = request.get_json()
         
-        response = requests.post(PREPROCESSOR_URL, json=raw_data)
+        prep_response = requests.post(PREPROCESSOR_URL, json=raw_data)
         
-        if response.status_code != 200:
+        if prep_response.status_code != 200:
             return jsonify({"error": "Failed at preprocessing stage"}), 500
             
-        result = response.json()
+        cleaned_data = prep_response.json()
+
+        ai_response = requests.post(PREDICTION_URL, json=cleaned_data)
+        
+        if ai_response.status_code != 200:
+            return jsonify({"error": "Failed at AI prediction stage"}), 500
+
+        ai_result = ai_response.json()
+
         return jsonify({
-            "message": "Transaction processed successfully through microservices pipeline",
-            "pipeline_result": result
+            "message": "Transaction analyzed successfully through pipeline",
+            "is_fraud": ai_result.get("is_fraud"),
+            "message": "Fraudulent Transaction Detected" if ai_result.get("is_fraud") == 1 else "Transaction Approved"
         }), 200
+
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
@@ -38,7 +47,6 @@ def upload_csv():
     
     try:
         files = {'file': (file.filename, file.stream, file.content_type)}
-        
         response = requests.post(PREPROCESSOR_CSV_URL, files=files)
         
         return jsonify(response.json()), response.status_code
